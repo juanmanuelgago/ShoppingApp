744b682c9e808286b34ecbba901b1d5bdcf5bce8
Juan Manuel Gago <jmgago279@gmail.com>
2019-05-07 00:03:41 -0300

    change constraints in banner, style banner image in corners

--- ShoppingApp/BannerCollectionViewCell.swift
+++ ShoppingApp/BannerCollectionViewCell.swift
@@ -14,4 +14,14 @@ class BannerCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        styleImage()
+    }
+    
+    func styleImage() {
+        bannerImageView.layer.cornerRadius = 5
+        bannerImageView.clipsToBounds = true
+    }
+    
 }
